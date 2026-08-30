e687244fe50b06cbfd718167b2ab7530cbd80b3f
Skit-025 <codesheritage@gmail.com>
2026-08-31 01:13:08 +0530

    Update sw.js

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jtli0hcvoco"
+    "revision": "0.253fk618g9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
